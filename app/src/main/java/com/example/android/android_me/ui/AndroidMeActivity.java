c27ef84a29016bfeb7f07b4e0a8212559c8dfe00
Angel <angelgutlop@gmail.com>
2018-04-25 10:56:13 +0200

    Añadidos fragmetos para las diferentes partes del cuerpo y la posibilidad de generar un personaje aleatorio

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -1,32 +1,115 @@
 /*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.android.android_me.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
 
 // This activity will display a custom Android image composed of three body parts: head, body, and legs
 public class AndroidMeActivity extends AppCompatActivity {
+//Estas variables se reinicializan al girar
+
+    private BodyFragment headFragment;
+    private BodyFragment bodyFragment;
+    private BodyFragment legsFragment;
+
+    private final String headIdTag = "headid";
+    private final String bodyIdTag = "bodyid";
+    private final String leggsIdTag = "legid";
+
+
+    FragmentManager fragmentManager = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_me);
+
+        fragmentManager = getSupportFragmentManager();
+
+
+        headFragment = (BodyFragment) fragmentManager.findFragmentByTag(headIdTag);
+        bodyFragment = (BodyFragment) fragmentManager.findFragmentByTag(bodyIdTag);
+        legsFragment = (BodyFragment) fragmentManager.findFragmentByTag(leggsIdTag);
+
+
+        if (headFragment == null) {
+
+            headFragment = new BodyFragment();
+            bodyFragment = new BodyFragment();
+            legsFragment = new BodyFragment();
+
+            headFragment.setBodyItemList(AndroidImageAssets.getHeads());
+            headFragment.setDisplayElementRandom();
+
+            bodyFragment.setBodyItemList(AndroidImageAssets.getBodies());
+            bodyFragment.setDisplayElementRandom();
+
+            legsFragment.setBodyItemList(AndroidImageAssets.getLegs());
+            legsFragment.setDisplayElementRandom();
+
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.headContainer, headFragment, headIdTag);
+            fragmentTransaction.add(R.id.bodyContainer, bodyFragment, bodyIdTag);
+            fragmentTransaction.add(R.id.legsContainer, legsFragment, leggsIdTag);
+
+            fragmentTransaction.commit();
+        }
+
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(headIdTag, headFragment.getDisplayElement());
+        outState.putInt(bodyIdTag, bodyFragment.getDisplayElement());
+        outState.putInt(leggsIdTag, legsFragment.getDisplayElement());
+
+
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.randomButton: {
+                this.generateRandomCharacter();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void generateRandomCharacter() {
+        headFragment.displayRandomElement();
+        bodyFragment.displayRandomElement();
+        legsFragment.displayRandomElement();
+    }
+
+
 }
---
+++ app/src/main/java/com/example/android/android_me/ui/BodyFragment.java
@@ -0,0 +1,149 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.android.android_me.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class BodyFragment extends Fragment implements View.OnClickListener {
+    List<Integer> bodyItemList = null;
+    private Integer nBody = 0;
+
+    private View bodyView;
+    private ImageView bodyImageView;
+
+
+    private static final String itemListID = "id_item_list";
+    private static final String nbodyID = "id_body_number";
+
+
+    public BodyFragment() {
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        if (savedInstanceState != null) {
+            nBody = savedInstanceState.getInt(nbodyID);
+            bodyItemList = savedInstanceState.getIntegerArrayList(itemListID);
+        }
+
+        bodyView = inflater.inflate(R.layout.fragment_body, container, false);
+        bodyImageView = (ImageView) bodyView.findViewById(R.id.body_imageView);
+
+        bodyImageView.setOnClickListener(this);
+
+        setImageView(nBody);
+
+
+        return bodyView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putInt(nbodyID, nBody);
+        outState.putIntegerArrayList(itemListID, (ArrayList<Integer>) bodyItemList);
+        super.onSaveInstanceState(outState);
+    }
+
+
+    private void setImageView(int n) {
+
+        if (bodyItemList != null) {
+            bodyImageView.setImageResource(bodyItemList.get(n));
+        }
+    }
+
+    public void setBodyItemList(List<Integer> bodyItemList) {
+        this.bodyItemList = bodyItemList;
+    }
+
+    public void setDisplayElement(int nBody) {
+        this.nBody = nBody;
+    }
+
+    public Integer getDisplayElement() {
+        return this.nBody;
+    }
+
+    Handler mHandler = new Handler();
+
+    volatile AtomicInteger cont = new AtomicInteger(0);
+
+    public void displayRandomElement() {
+
+
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                {
+                    mHandler.postDelayed(this, 50);
+
+                    nBody = getRandomElementIndex();
+                    setImageView(nBody);
+
+
+                    if (cont.incrementAndGet() >= 10) {
+                        cont.getAndSet(0);
+                        mHandler.removeCallbacks(this);
+                    }
+                }
+            }
+        };
+
+
+        mHandler.post(runnable);
+
+
+    }
+
+
+    public void setDisplayElementRandom() {
+        this.nBody = getRandomElementIndex();
+    }
+
+    public Integer getRandomElementIndex() {
+
+        if (bodyItemList == null) return null;
+        Random random = new Random();
+        return random.nextInt(bodyItemList.size() - 1);
+    }
+
+    public Integer getNextListItemIndex() {
+        if (nBody == null) nBody = 0;
+        else nBody++;
+
+        if (nBody > bodyItemList.size() - 1) nBody = 0;
+        return nBody;
+    }
+
+    @Override
+    public void onClick(View v) {
+        nBody = getNextListItemIndex();
+        setImageView(nBody);
+    }
+
+
+//
+
+
+}
